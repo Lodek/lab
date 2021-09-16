827dc1156d1bbac4ff780fcc54ecdc503786c566
Bruno Gomes <gomes.bruno.ac@gmail.com>
2021-09-16 14:11:39 -0300

    tested alternators and removed unused code

--- rust/parser-combinators/src/combinators.rs
+++ rust/parser-combinators/src/combinators.rs
@@ -1,50 +1,29 @@
 use super::{Parser, ParserResult, ParserT};
 
-
-pub fn sequence<'a, P, T>(stream: &'a str, parsers: P) -> ParserResult<'a, Vec<T>>
-where P: Iterator<Item=Parser<'a, T>>
-{
-    fn chaining_function<T>(mut vec: Vec<T>, parsed_value: T) -> Vec<T> {
-        vec.push(parsed_value);
-        vec
-    }
-
-    parsers.fold(Ok((Vec::new(), stream)), |result, parser| {
-        chain(result, parser, chaining_function)
-    })
-}
-
+// TODO implement sequence macro
 
 // FIXME alternation should take an arbitrary number of parser
-pub fn alternation<'a, T>(stream: &'a str, main: Parser<'a, T>, other: Parser<'a, T>) -> ParserResult<'a, T> {
+pub fn alternation<'a, T>(
+    stream: &'a str,
+    main: Parser<'a, T>,
+    other: Parser<'a, T>,
+) -> ParserResult<'a, T> {
     match (main)(stream) {
         Err(_) => (other)(stream),
         result => result,
     }
 }
 
-pub fn chain<'a, T, U, F, V>(parser_result: ParserResult<'a, T>, parser: Parser<'a, U>, combiner: F) -> ParserResult<'a, V>
-where F: Fn(T, U) -> V
-{
-    match parser_result {
-        Ok((first, tail)) => {
-            match parser(tail) {
-                Ok((second, tail)) => Ok((combiner(first, second), tail)),
-                Err(msg) => Err(msg)
-            }
-        }
-        Err(msg) => Err(msg)
-    }
-}
-
 pub fn many<'a, T>(stream: &'a str, parser: Parser<'a, T>) -> ParserResult<'a, Vec<T>> {
-
-    fn recursion<'b, U>(stream: &'b str, parser: Parser<'b, U>, mut acc: Vec<U>) -> ParserResult<'b, Vec<U>> {
+    fn recursion<'b, U>(
+        stream: &'b str,
+        parser: Parser<'b, U>,
+        mut acc: Vec<U>,
+    ) -> ParserResult<'b, Vec<U>> {
         if let Ok((value, tail)) = (parser)(stream) {
             acc.push(value);
             return recursion(tail, parser, acc);
-        }
-        else {
+        } else {
             return Ok((acc, stream));
         }
     }
@@ -55,12 +34,10 @@ pub fn many<'a, T>(stream: &'a str, parser: Parser<'a, T>) -> ParserResult<'a, V
 
 pub fn some<'a, T>(stream: &'a str, parser: Parser<'a, T>) -> ParserResult<'a, Vec<T>> {
     match (parser)(stream) {
-        Ok((value, tail)) => {
-            many(tail, parser).map(|(mut vec, tail)| {
-                    vec.insert(0, value);
-                    (vec, tail)
-            })
-        }
-        Err(msg) => Err(format!("No sucessful parse in `some`: {}", msg))
+        Ok((value, tail)) => many(tail, parser).map(|(mut vec, tail)| {
+            vec.insert(0, value);
+            (vec, tail)
+        }),
+        Err(msg) => Err(format!("No sucessful parse in `some`: {}", msg)),
     }
 }
--- rust/parser-combinators/src/parsers.rs
+++ rust/parser-combinators/src/parsers.rs
@@ -6,36 +6,32 @@ pub fn elem<'a>(stream: &'a str) -> ParserResult<'a, char> {
     let mut chars = stream.chars();
     match chars.next() {
         None => Err(String::from("Empty stream")),
-        Some(c) => Ok((c, &stream[1..]))
+        Some(c) => Ok((c, &stream[1..])),
     }
 }
 
-pub fn satisfies<'a, F>(stream: &'a str, predicate: F) -> ParserResult<'a, char> 
-where F: Fn(char) -> bool 
+pub fn satisfies<'a, F>(stream: &'a str, predicate: F) -> ParserResult<'a, char>
+where
+    F: Fn(char) -> bool,
 {
     match elem(stream) {
         Err(msg) => Err(msg),
-        Ok((c, tail)) => {
-            match predicate(c) {
-                true => Ok((c, tail)),
-                _ => Err(String::from("Predicate not satisfied"))
-            }
-        }
+        Ok((c, tail)) => match predicate(c) {
+            true => Ok((c, tail)),
+            _ => Err(String::from("Predicate not satisfied")),
+        },
     }
 }
 
-pub fn digit<'a>(stream: &'a str) -> ParserResult<'a, char> 
-{
+pub fn digit<'a>(stream: &'a str) -> ParserResult<'a, char> {
     satisfies(stream, |c| c.is_ascii_digit())
 }
 
-pub fn alphabetic<'a>(stream: &'a str) -> ParserResult<'a, char> 
-{
+pub fn alphabetic<'a>(stream: &'a str) -> ParserResult<'a, char> {
     satisfies(stream, |c| c.is_ascii_alphabetic())
 }
 
-pub fn alphanumeric<'a>(stream: &'a str) -> ParserResult<'a, char> 
-{
+pub fn alphanumeric<'a>(stream: &'a str) -> ParserResult<'a, char> {
     satisfies(stream, |c| c.is_ascii_alphanumeric())
 }
 
