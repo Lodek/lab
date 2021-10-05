8b4f47bff9d5c138c3f869e0697379f8083e6604
Bruno Gomes <gomes.bruno.ac@gmail.com>
2021-10-05 10:11:07 -0300

    refactor: grep main

--- rust/grep-parser-combinators/src/main.rs
+++ rust/grep-parser-combinators/src/main.rs
@@ -1,20 +1,32 @@
-use parser_combinators::parsers::elem;
 use std::io as io;
+use std::env::args;
 
+use parser_combinators::parsers::elem;
 
 use grep_parser_combinators::regex_parser::parse_expression;
 use grep_parser_combinators::transformers::expression_parser_factory;
 
 fn main() {
-    //let mut stream = String::new();
-    //let mut stdin = io::stdin();
-    //stdin.read_line(&mut stream).unwrap();
+    let mut args_iter = args();
+    args_iter.next();
+    let regex = args_iter.next().unwrap();
 
-    let regex = "ab(c|d)*";
-
-    let (expr, tail) = parse_expression(regex).unwrap();
+    let (expr, tail) = parse_expression(&regex).unwrap();
     let parser = expression_parser_factory(&expr);
-    
-    let result = parser("abcccccccc");
-    println!("{:?}", result);
+
+    let mut stream = String::new();
+    let stdin = io::stdin();
+
+    loop {
+        match stdin.read_line(&mut stream) {
+            Ok(0) => break,
+            Ok(_) => {
+                if let Ok(_) = parser(&stream) {
+                    println!("{}", stream);
+                }
+                stream.clear();
+            }
+            Err(_) => break,
+        }
+    }
 }
